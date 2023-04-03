01108cb33aeca5000a105a7a63677436347b5f30
Mabel <luccadacambra@gmail.com>
2023-04-03 15:00:00 +0000

    Minor Changes

--- Assets/Scripts/Editor/CutsceneEditor.cs
+++ Assets/Scripts/Editor/CutsceneEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26fa0f52cc3bb61fee337f1527affd29232e7227fc1594100df183c79ff33dd7
-size 7849
+oid sha256:a67d222dd2ce7868c6bdd3f0e131b55ff2fb527fbcb906ad727fb72a4b595440
+size 7475
