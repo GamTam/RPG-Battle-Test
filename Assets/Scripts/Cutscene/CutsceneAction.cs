3b23ebfb36023415a720fe7c5b0bd6ce496a6b8a
IIGhostPantsII <david.cacorovski000@gmail.com>
2023-04-06 04:35:32 -0400

    Changes

--- Assets/Scripts/Cutscene/CutsceneAction.cs
+++ Assets/Scripts/Cutscene/CutsceneAction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7f16782fe28cc3d890eaf934966265dea6cfb32d7c550b3978b08d487e2d381
-size 5290
+oid sha256:98d0fda3fbf5b16601a9168e7677f75c30ec1d7ffb75477b6a6cf36445ad62d6
+size 5289
