751a387bb6acf5efcf0c6c8dcf15a0b8cf6df102
Mabel <luccadacambra@gmail.com>
2023-05-23 17:52:12 -0400

    Fixed Saving/Loading

--- Assets/Scripts/Overworld/FollowerController.cs
+++ Assets/Scripts/Overworld/FollowerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9da8aef7b73f7b582dec583ceb714ac07edfe5ea47ca4f3fcf54416a396b9045
-size 1743
+oid sha256:93226805fdeb15e770b400938bc6459d92aa8177e63c71160be8d74be8a91358
+size 2154
